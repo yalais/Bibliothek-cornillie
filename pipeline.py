59a415fe8cb439718b79b18b419742b09810536b
yalais <yannic.lais@students.fhnw.ch>
2023-05-17 15:30:51 +0200

    minor changes analysis, create pipeline

---
+++ pipeline.py
@@ -0,0 +1,24 @@
+import os
+import subprocess
+
+def run_notebook(notebook_path):
+    command = [
+        "jupyter",
+        "nbconvert",
+        "--to=notebook",
+        "--execute",
+        notebook_path,
+        "--inplace"
+    ]
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    if process.returncode != 0:
+        raise Exception(f"Fehler beim Ausführen des Notebooks. {stderr}")
+
+# Path to the notebooks
+notebook_path1 = "scraper/scraper_blatter.ipynb"
+notebook_path2 = "scraper/scraper_buchhaus.ipynb"
+
+run_notebook(notebook_path1)
+run_notebook(notebook_path2)
